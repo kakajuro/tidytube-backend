cd563535a3804119512af2eebc3df25d3ab7c0bf
swishyy <famodavid1@gmail.com>
2024-07-12 16:05:31 +0100

    Hero component initialised

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-center">
-      Hello
+    <main className="flex min-h-screen w-screen">
+      <Hero />
     </main>
   );
 }
---
+++ web/components/Hero.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Hero() {
+  return (
+    <section className="flex flex-col w-screen min-h-screen items-center justify-center pb-40">
+      <h1 className="font-semibold text-5xl sm:text-7xl md:text-8xl">Youtube but <a className="bg-gradient-to-r from-red-500 via-red-600 to-red-500 text-transparent bg-clip-text">better.</a></h1>
+    </section>
+  )
+}
