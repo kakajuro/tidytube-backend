09fa9f0beb78d3e1ff2ae967df38ce9ae817765c
swishyy <famodavid1@gmail.com>
2024-07-15 17:59:10 +0100

    Made mini about section responsive

--- web/components/MiniAbout.tsx
+++ web/components/MiniAbout.tsx
@@ -1,26 +1,29 @@
+"use client";
+
 import Image from "next/image"
+import { redirect } from "next/navigation";
 
 import { BorderBeam } from "@/components/magicui/border-beam";
+import { synopsisForMiniAbout } from "@/components/text";
 
-import tidytubeScreenshot from "../app/tidytubescreenshot.jpg"
-
-import { synopsisForMiniAbout } from "@/components/text"
+import tidytubeScreenshot from "../app/tidytubescreenshot.png";
 
 export default function MiniAbout() {
 
   return (
-    <section className="min-h-screen px-48 flex flex-col items-center justify-center">
-      <h1 className="font-semibold text-4xl pb-24">what is tidytube?</h1>
-      <div className="grid place-content-center grid-cols-2 text-white">
-        <div className="flex flex-col max-w-[480px] justify-center">
-          <span className="text-xl pb-8">{synopsisForMiniAbout}</span>
-          <span className="text-xl underline hover:cursor-pointer hover:text-red-700 transition-all ease-in-out max-w-[290px] h-7">Find out more about how it works:</span>
+    <section className="min-h-screen lg:px-48 flex flex-col items-center justify-center select-none">
+      <h1 className="font-semibold text-4xl pb-12 lg:pb-20">what is tidytube?</h1>
+      <div className="grid place-content-center grid-cols-1 lg:grid-cols-2 text-white">
+        <div className="flex flex-col pb-10 lg:pb-0 lg:pr-12 xl:pr-0 max-w-[350px] sm:max-w-[480px] justify-center text-center lg:text-left">
+          <span className="text-lg sm:text-xl pb-8">{synopsisForMiniAbout}</span>
+          <span className="text-lg sm:text-xl underline hover:cursor-pointer hover:text-red-700 transition-all ease-in-out" onClick={() => redirect("/about")}>Find out more about how it works:</span>
         </div>
         <div className="justify-self-center place-self-center">
-          <div className="relative border-beam rounded-lg">
+          <div className="relative border-beam rounded-lg drop-shadow-glow">
             <Image 
               src={tidytubeScreenshot}
               alt="Screenshot of tidytube browser extension" 
+              className="rounded-lg"
               placeholder="blur"
             />
             <BorderBeam />
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -26,6 +26,12 @@ const config = {
         "custom-dark": "#09090B",
         "custom-grey": "#96969E"
       },
+      dropShadow: {
+        glow: [
+          "0 0px 20px rgba(255,255, 255, 0.15)",
+          "0 0px 65px rgba(255, 255,255, 0.05)"
+        ]
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
