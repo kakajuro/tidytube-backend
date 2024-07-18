8eabb31ad97f5889100f4434a48f468914d9dea4
swishyy <famodavid1@gmail.com>
2024-07-18 21:01:20 +0100

    Donation page added except links

--- web/app/donate/page.tsx
+++ web/app/donate/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { Metadata } from "next";
 
+import { donationMessage, donationMessageTwo } from "@/components/text";
+
 export const metadata: Metadata = {
   title: "Donate - tidytube"
 }
@@ -8,7 +10,25 @@ export const metadata: Metadata = {
 export default function page() {
   return (
     <main className="flex flex-col items-center min-h-screen w-screen p-12">
-      Donate
+      <div className="flex flex-col w-[90%] md:w-[60%] lg:w-[50%] xl:w-[40%]">
+        <h1 className="text-5xl font-semibold pb-8">donate</h1>
+        <p className="font-medium text-xl pb-4">{donationMessage}</p>
+        <p className="font-medium text-xl pb-4">{donationMessageTwo}</p>
+        <div className="flex flex-col pt-4 text-xl">
+          <h2 className="font-semibold text-3xl pb-6">recurring:</h2>
+          <ul>
+            <li>-Github</li>
+          </ul>
+        </div>
+        <div className="flex flex-col pt-12 text-xl">
+          <h2 className="font-semibold text-3xl pb-6">one-time:</h2>
+          <ul>
+            <li>-Github</li>
+            <li>-Ko-fi</li>
+            <li>-Paypal</li>
+          </ul>
+        </div>
+      </div>
     </main>
   )
 }
--- web/components/text.js
+++ web/components/text.js
@@ -8,3 +8,10 @@ When you are on a Youtube page the extension listens for DOM updates on the page
 export const dataNoticeAbout = `
 The extension keeps track of the sections removed on each page and displays them in the extension popup. Very little data is stored when using this extension, find out exactly what data is stored 
 `
+
+export const donationMessage = `
+I'm David (kakajuro), a first year university student and I've worked on the tidytube extension in my free time. 
+`
+
+export const donationMessageTwo = `
+Donations are absolutely not required but are very much appreciated. Any form of donation, recurring or one-time is greatly appreciated. Consider supporting with one of the options provided below. I am happy to provide more payment methods in future if the demand for it is high enough.`
