2d90af594a58c3bfb8b7a709c92b082834036a89
swishyy <famodavid1@gmail.com>
2024-07-18 13:06:59 +0100

    Added credits page

---
+++ web/app/credits/page.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function page() {
+  return (
+    <main className="flex flex-col items-center min-h-screen w-screen p-12">
+      <div className="flex flex-col w-[90%] md:w-[50%] items-center">
+        <h1 className="text-5xl font-semibold pb-8">credits:</h1>
+        <ul className="text-xl">
+          <li className="pb-4">
+            Created by: <Link className="hover:underline hover:cursor-pointer" href="https://github.com/kakajuro" passHref target="_blank">@kakajuro</Link>
+          </li>
+          <li className="pb-4">
+            <Link className="hover:underline hover:cursor-pointer" href="https://sponsor.ajay.app" passHref target="_blank">Sponsorblock </Link> 
+            (<Link className="hover:underline hover:cursor-pointer" href="https://ajay.app/" passHref target="_blank">Ajay Ramachandran</Link>) 
+            - Although I am not directly affiliated with Ajay or SponsorBlock, I took alot of inspiration from this extension.</li>
+          <li className="pb-4">All icons provided by <Link className="hover:underline hover:cursor-pointer" href="https://react-icons.github.io/react-icons/" passHref target="_blank">react-icons</Link></li>
+          <li className="pb-4">Logo: <Link className="hover:underline hover:cursor-pointer" href="https://github.com/kakajuro" passHref target="_blank">@kakajuro</Link></li>
+        </ul>
+      </div>
+    </main>
+  )
+}
