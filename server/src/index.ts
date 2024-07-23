11ca82ebc45f044718621c970c0069f073debc68
swishyy <famodavid1@gmail.com>
2024-07-23 15:57:24 +0100

    Added middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,28 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
+import dotenv from "dotenv";
 dotenv.config();
 
-const app: Express = express();
 const port = process.env.PORT;
+const env = process.env.NODE_ENV;
 
-app.get("/", (req: Request, res: Response) => {
-  console.log(`Recieved Request: ${req.ip}`)
-  res.send("API working...");
-});
+const app:Express = express();
 
-app.get("/test", (req: Request, res: Response) => {
-  res.send("test route!");
+app.use(helmet());
+if (env == "development") {
+  app.use(morgan("dev"));
+} else {
+  app.use(morgan("tiny"));
+}
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api", (req: Request, res: Response, next: NextFunction) => {
+  return res.send({"result": "API working..."});
 });
 
 app.listen(port, () => {
