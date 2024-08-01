df65e4a1aa6f92104f35b904eb73deef6b3387e9
swishyy <famodavid1@gmail.com>
2024-08-01 11:10:20 +0100

    Remove icon

--- web/components/Hero.tsx
+++ web/components/Hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
       <BlurFade className="w-[50%] text-2xl xl:pb-4 xl:pt-10" delay={1.2} duration={0.6}>
         <h2 className="pt-20 text-4xl font-semibold bg-gradient-to-tr from-white via-slate-300 text-transparent bg-clip-text">Download</h2>
       </BlurFade>
-      <div className="flex flex-row justify-evenly items-center pt-10 h-8 w-[80%] sm:w-[60%] md:w-[50%] lg:w-[40%]">
+      <div className="flex flex-row justify-evenly items-center pt-10 h-8 w-[80%] sm:w-[60%] md:w-[60%] lg:w-[70%]">
         <BlurFade delay={baseDelayIcons + 0.1} duration={0.2} inline={false}>
           <BsBrowserChrome className="hover:cursor-pointer hover:text-red-600 hover:-translate-y-1 duration-500 transition-all ease-in-out" size={35} title="Chrome Extension Download" />
         </BlurFade> 
@@ -40,9 +40,6 @@ export default function Hero() {
         </BlurFade>
         <BlurFade delay={baseDelayIcons + 0.3} duration={0.2} inline={false}>
           <BsBrowserEdge className="hover:cursor-pointer hover:text-red-600 hover:-translate-y-1 duration-500 transition-all ease-in-out" size={35} title="Edge Extension Download" />
-        </BlurFade>
-        <BlurFade delay={baseDelayIcons + 0.4} duration={0.2} inline={false}>
-          <FaOpera className="hover:cursor-pointer hover:text-red-600 hover:-translate-y-1 duration-500 transition-all ease-in-out" size={35} title="Opera Extension Download" />
         </BlurFade>
       </div>
     </section>
