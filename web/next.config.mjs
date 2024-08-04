faab2a9d6552f66a980552ba8333f4fc8b91e2f6
swishyy <famodavid1@gmail.com>
2024-08-04 15:26:46 +0100

    Edits made on prod server

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  staticPageGenerationTimeout: 1000,
   experimental: {
     missingSuspenseWithCSRBailout: false
   },
