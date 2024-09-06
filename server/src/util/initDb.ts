14d3e4e1cc3fc632a4f31a5c016f73d7ed661213
swishyy <famodavid1@gmail.com>
2024-09-06 23:35:07 +0100

    Added new features to stats data model

--- server/src/util/initDb.ts
+++ server/src/util/initDb.ts
@@ -21,6 +21,8 @@ export interface PageChangeData {
   "removeShortsPlayback": number,
   "removeShortsRemixingThisVideo": number,
   "removeShortsWhileWatching": number
+  "removeRecommendedTopicsFromSearch": number,
+  "autoDisableAutoplay": number
 }
 
 export const defaultStats: PageChangeData = {
@@ -43,29 +45,48 @@ export const defaultStats: PageChangeData = {
   removeShortsFromSearch: 0,
   removeShortsPlayback: 0,
   removeShortsRemixingThisVideo: 0,
-  removeShortsWhileWatching: 0
+  removeShortsWhileWatching: 0,
+  removeRecommendedTopicsFromSearch: 0,
+  autoDisableAutoplay: 0
 };
 
 export async function initDB() {
 
-	// Create stats object if it doesnt exist already
-	await redisClient.exists("stats")
-	.then(exists => {
-		if (!exists) {
-      // If stats object does not exist create it
-      for (const [key, value] of Object.entries(defaultStats)) {
-        redisClient.hset("stats", key, value);
-      }
+  console.log("Initialising datase...")
 
-    } else {
-      // Even if it does exists, create the missing keys if there are any
-      for (const [key, value] of Object.entries(defaultStats)) {
-        redisClient.hexists("hash", key, exists => {
-          if (!exists) redisClient.hset("hash", key, value)
-        })
-      }
+  // Create stats object if it doesnt exist already
+  let exists = await redisClient.exists("stats");
 
+  if (!exists) {
+
+    // If stats object does not exist create it
+    console.log("Creating stats data model...")
+    for (const [key, value] of Object.entries(defaultStats)) {
+      redisClient.hset("stats", key, value);
     }
-  })
+
+  } else {
+
+    // Even if it does exists, create the missing keys if there are any
+    for (const [key, value] of Object.entries(defaultStats)) {
+      redisClient.hexists("stats", key, (err, exists) => {
+        if (err) {
+          console.log("Error checking if key exists...")
+        } else {
+
+          if (!exists) { 
+            redisClient.hset("stats", key, value); 
+            console.log(`Added ${key} to stats data model`);
+          }
+
+        }
+
+      });
+    
+    }
+
+  }
+
+  console.log("Database initialised")
 
 }
